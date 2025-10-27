5378fc121602133e8e0256a2dd9f8478f3a18d26
lazy-b-goose <lazybaregoose@gmail.com>
2025-10-27 16:12:21 +0300

    Add files via upload

---
+++ EchoPlay/EchoPlay.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <bass.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    std::cout << "Hello world" << std::endl;
+
+    // Инициализация BASS
+    if (!BASS_Init(-1, 44100, 0, 0, NULL)) {
+        printf("Ошибка инициализации BASS\n");
+        return 1;
+    }
+
+    // Загрузка трека
+    HSTREAM stream = BASS_StreamCreateFile(FALSE, "audio\\POLMATERI_-_YArche_zvjozd_73991388.mp3", 0, 0, 0);
+    if (!stream) {
+        printf("Ошибка загрузки трека\n");
+        BASS_Free();
+        return 1;
+    }
+
+    // Воспроизведение
+    BASS_ChannelPlay(stream, FALSE);
+
+    // Ожидание завершения (или можно использовать цикл)
+    printf("Воспроизведение... Нажмите Enter для выхода\n");
+    getchar();
+
+    // Очистка
+    BASS_Free();
+    return 0;
+
+    return 0;
+}
