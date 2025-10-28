b71aec19eebbb67ec0afab44ca04ca5dfc487ba8
lazy-b-goose <lazybaregoose@gmail.com>
2025-10-28 22:58:11 +0300

    регулировка громкости

--- EchoPlay/EchoPlay.cpp
+++ EchoPlay/EchoPlay.cpp
@@ -26,25 +26,66 @@ int main() {
     // Воспроизведение
     BASS_ChannelPlay(stream, TRUE);
 
-    // Ожидание завершения
-
+    // Управление
+    float val = 0.1f;
+    BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, val);
     cout << "Воспроизведение... Нажмите Enter для выхода\n";
     while (BASS_ChannelIsActive(stream)) {                                          //пока воспроизводиться                                                     //нажат ли пробел
-        if (GetAsyncKeyState(VK_SPACE) & 0x8000) {                                  //нажатие пробела  
-            if (BASS_ChannelIsActive(stream) == BASS_ACTIVE_PLAYING) {          //трек играет
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000) {                                      //нажатие пробела  
+            if (BASS_ChannelIsActive(stream) == BASS_ACTIVE_PLAYING) {                      //трек играет
                 BASS_ChannelPause(stream);
                 cout << "пауза\n";
             }
-            else if (BASS_ChannelIsActive(stream) == BASS_ACTIVE_PAUSED) {          //трек не играет
+            else if (BASS_ChannelIsActive(stream) == BASS_ACTIVE_PAUSED) {                  //трек не играет
                 BASS_ChannelPlay(stream, FALSE);
                 cout << "продолжить\n";
             }
-        Sleep(100);
+            Sleep(150);
         }
-        if (GetAsyncKeyState(VK_RETURN) & 0x8000) {                             //нажат ли Enter
+        Sleep(20);
+        if (GetAsyncKeyState(VK_RETURN) & 0x8000) {                                     //нажатие Enter
             return 0;
         }
-        Sleep(100);
+        Sleep(20);
+        if (GetAsyncKeyState(VK_ADD) & 0x8000) {                                        //нажатие +
+            if (val < 1) {
+                val += 0.1f;
+                BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, val);;
+            }
+            else {
+                cout << "Максимальная громкость\n";
+            }
+        }
+        Sleep(20);
+        if (GetAsyncKeyState(VK_SUBTRACT) & 0x8000) {                                   //нажатие -
+            if (val > 1e-6) {
+                val -= 0.1f;
+                BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, val);
+            }
+            else {
+                cout << "Минимальная громкость\n";
+            }
+        }
+        Sleep(20);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
